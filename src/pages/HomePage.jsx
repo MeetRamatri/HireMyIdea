42f6b1c41767452457eab151daf171348f92a85e
MeetRamatri <meetramatri12@gmail.com>
2026-04-26 21:00:00 +0530

    setup react router v6 and homepage component shell

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="min-h-screen bg-neutral-cream font-sans">
+      {/* Navbar Section */}
+      
+      {/* Hero Section */}
+      
+      {/* HowItWorks Section */}
+      
+      {/* Features Section */}
+      
+      {/* Testimonials Section */}
+      
+      {/* Pricing Section */}
+      
+      {/* AppDownload Section */}
+      
+      {/* Footer Section */}
+    </div>
+  );
+};
+
+export default HomePage;
